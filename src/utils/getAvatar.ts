c41ebf5c88bfa6e8c8b710ffed02d80227990053
Allenyou <i@allenyou.wang>
2024-10-25 21:11:32 +0800

    feat: add size param for getAvatar

--- src/utils/getAvatar.ts
+++ src/utils/getAvatar.ts
@@ -1,6 +1,9 @@
 import { config } from "@/data/site-config";
 import md5 from "md5-ts";
 
-export default function getAvatar(email: string = "test@example.com"): string {
-	return `${config.gravatar_mirror}${md5(email)}`;
+export default function getAvatar(
+	email: string = "test@example.com",
+	size: number = 80
+): string {
+	return `${config.gravatar_mirror}${md5(email)}?s=${size}`;
 }
