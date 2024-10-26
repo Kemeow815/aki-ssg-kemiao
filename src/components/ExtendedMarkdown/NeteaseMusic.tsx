0e2d41e596db9e056e99b70547d54d441e44a790
Allenyou <i@allenyou.wang>
2024-10-26 16:48:14 +0800

    feat: add Netease Music directive(not implemented yet)

---
+++ src/components/ExtendedMarkdown/NeteaseMusic.tsx
@@ -0,0 +1,15 @@
+import { NeteaseMusicClient } from "./NeteaseMusicClient";
+
+export default async function NeteaseMusic({ id }: { id: number }) {
+	throw new Error("Not Implemented yet.");
+	// return (
+	// 	<NeteaseMusicClient
+	// 		cover={detail.data.album.cover}
+	// 		name={detail.data.name}
+	// 		artists={detail.data.artists.map((v) => {
+	// 			return v.name;
+	// 		})}
+	// 		url={url.data.url}
+	// 	/>
+	// );
+}
