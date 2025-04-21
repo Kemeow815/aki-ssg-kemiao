b932f9c42b5541d7e104832032ee8e16ff38a3a1
Allenyou <i@allenyou.wang>
2025-04-21 16:45:52 +0800

    refactor: rewrite content management module

--- src/libs/content-management.ts
+++ src/libs/content-management.ts
@@ -6,133 +6,152 @@ import { cache } from "react";
 import { MarkdownContent } from "./markdown-render";
 import { isProd } from "./state-management";
 
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type MetadataParser<MetadataType> = (attr: any) => MetadataType;
+
+function defineMetadataParser<MetadataType>(
+	parser: (attr: unknown) => MetadataType
+) {
+	return (attr: unknown) => {
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		return parser(attr as any);
+	};
+}
+
+const post_metadata_parser = defineMetadataParser<PostMetadata>(
+	(
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		attr: any
+	) => {
+		const id_str = attr.id;
+		if (id_str === undefined) {
+			throw new Error(`Please specify an ID for post!`);
+		}
+		const id = parseInt(id_str);
+		if (isNaN(id)) {
+			throw new Error(`Invalid post ID ${id_str}`);
+		}
+		const title = attr.title;
+		if (title === undefined) {
+			throw new Error(`Please specify a title for post!`);
+		}
+		return {
+			id,
+			title,
+			description: attr.description ?? "暂无描述",
+			modified_at: new Date(attr.modified_at ?? "1919-08-10T11:45:14Z"),
+			draft:
+				attr.draft === undefined || typeof attr.draft !== "boolean"
+					? false
+					: attr.draft,
+		};
+	}
+);
+const page_metadata_parser = defineMetadataParser<PageMetadata>(
+	(
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		attr: any
+	) => {
+		const slug = attr.slug;
+		if (slug === undefined) {
+			throw new Error(`Please specify a slug for page!`);
+		}
+		const title = attr.title;
+		if (title === undefined) {
+			throw new Error(`Please specify a title for page!`);
+		}
+		return {
+			slug,
+			title,
+			enable_comment:
+				attr.enable_comment === undefined ||
+				typeof attr.enable_comment !== "boolean"
+					? false
+					: attr.enable_comment,
+			allow_index:
+				attr.allow_index === undefined || typeof attr.allow_index !== "boolean"
+					? false
+					: attr.allow_index,
+			navigation_title: attr.navigation_title,
+			navigation_index:
+				attr.navigation_index === undefined
+					? 0
+					: ((v) => {
+							const id = parseInt(v);
+							if (isNaN(id)) {
+								throw new Error();
+							}
+							return id;
+					  })(attr.navigation_index),
+			draft:
+				attr.draft === undefined || typeof attr.draft !== "boolean"
+					? false
+					: attr.draft,
+		};
+	}
+);
+
 class CMS {
 	private friend_links: FriendLink[] = [];
 	private posts: Post[] = [];
 	private pages: Page[] = [];
+
+	private async parse_markdown_file<MetadataType>(
+		path: string,
+		metadata_parser: MetadataParser<MetadataType>
+	): Promise<MetadataType & Content> {
+		return await fs
+			.readFile(path)
+			.then((file) => {
+				return fm(file.toString());
+			})
+			.then((data) => {
+				const metadata = metadata_parser(data.attributes);
+				return {
+					...metadata,
+					original_content: data.body,
+					markdown_content: new MarkdownContent(data.body),
+				};
+			});
+	}
+	private async process_all_file<T>(
+		files: string[],
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		metadata_parser: (attr: any) => T
+	) {
+		return Promise.all(
+			files
+				.filter((filename) => filename.endsWith(".md"))
+				.map((filename) => this.parse_markdown_file(filename, metadata_parser))
+		);
+	}
+	private async init_posts() {
+		const posts_path = path.join(process.cwd(), "src", "data", "posts");
+		await fs
+			.readdir(posts_path)
+			.then((files) => files.map((file) => path.join(posts_path, file)))
+			.then((paths) => this.process_all_file(paths, post_metadata_parser))
+			.then((list) => {
+				this.posts = list
+					.filter((v) => !isProd || !v.draft)
+					.sort((a, b) => {
+						return b.modified_at.getTime() - a.modified_at.getTime();
+					});
+			});
+	}
+	private async init_pages() {
+		const pages_path = path.join(process.cwd(), "src", "data", "pages");
+		await fs
+			.readdir(pages_path)
+			.then((files) => files.map((file) => path.join(pages_path, file)))
+			.then((paths) => this.process_all_file(paths, page_metadata_parser))
+			.then((list) => {
+				this.pages = list.filter((v) => !isProd || !v.draft);
+			});
+	}
 	async init() {
 		this.friend_links = friend_link_list;
-		await Promise.all([
-			fs
-				.readdir(path.join(process.cwd(), "src", "data", "posts"))
-				.then((files) => {
-					return Promise.all(
-						files
-							.filter((filename) => filename.endsWith(".md"))
-							.map(async (filename) => {
-								return await fs
-									.readFile(
-										path.join(process.cwd(), "src", "data", "posts", filename)
-									)
-									.then((file) => {
-										return fm(file.toString());
-									})
-									.then((data) => {
-										// eslint-disable-next-line @typescript-eslint/no-explicit-any
-										const attr = data.attributes as any;
-										const id_str = attr.id;
-										if (id_str === undefined) {
-											throw new Error(`Please specify an ID for post!`);
-										}
-										const id = parseInt(id_str);
-										if (isNaN(id)) {
-											throw new Error(`Invalid post ID ${id_str}`);
-										}
-										const title = attr.title;
-										if (title === undefined) {
-											throw new Error(`Please specify a title for post!`);
-										}
-										return {
-											id,
-											title,
-											original_content: data.body,
-											markdown_content: new MarkdownContent(data.body),
-											description: attr.description ?? "暂无描述",
-											modified_at: new Date(
-												attr.modified_at ?? "1919-08-10T11:45:14Z"
-											),
-											draft:
-												attr.draft === undefined ||
-												typeof attr.draft !== "boolean"
-													? false
-													: attr.draft,
-										};
-									});
-							})
-					);
-				})
-				.then((list) => {
-					this.posts = list
-						.filter((v) => !isProd || !v.draft)
-						.sort((a, b) => {
-							return b.modified_at.getTime() - a.modified_at.getTime();
-						});
-				}),
-			fs
-				.readdir(path.join(process.cwd(), "src", "data", "pages"))
-				.then((files) => {
-					return Promise.all(
-						files
-							.filter((filename) => filename.endsWith(".md"))
-							.map(async (filename) => {
-								return await fs
-									.readFile(
-										path.join(process.cwd(), "src", "data", "pages", filename)
-									)
-									.then((file) => {
-										return fm(file.toString());
-									})
-									.then((data) => {
-										// eslint-disable-next-line @typescript-eslint/no-explicit-any
-										const attr = data.attributes as any;
-										const slug = attr.slug;
-										if (slug === undefined) {
-											throw new Error(`Please specify a slug for page!`);
-										}
-										const title = attr.title;
-										if (title === undefined) {
-											throw new Error(`Please specify a title for page!`);
-										}
-										return {
-											slug,
-											title,
-											original_content: data.body,
-											markdown_content: new MarkdownContent(data.body),
-											enable_comment:
-												attr.enable_comment === undefined ||
-												typeof attr.enable_comment !== "boolean"
-													? false
-													: attr.enable_comment,
-											allow_index:
-												attr.allow_index === undefined ||
-												typeof attr.allow_index !== "boolean"
-													? false
-													: attr.allow_index,
-											navigation_title: attr.navigation_title,
-											navigation_index:
-												attr.navigation_index === undefined
-													? 0
-													: ((v) => {
-															const id = parseInt(v);
-															if (isNaN(id)) {
-																throw new Error();
-															}
-															return id;
-													  })(attr.navigation_index),
-											draft:
-												attr.draft === undefined ||
-												typeof attr.draft !== "boolean"
-													? false
-													: attr.draft,
-										};
-									});
-							})
-					);
-				})
-				.then((list) => {
-					this.pages = list.filter((v) => !isProd || !v.draft);
-				}),
-		]);
+		await Promise.all([this.init_posts(), this.init_pages()]);
 	}
 	getFriendLinks() {
 		return this.friend_links;
--- src/libs/types/content.d.ts
+++ src/libs/types/content.d.ts
@@ -1,10 +1,10 @@
 declare type Content = {
-	title: string;
 	original_content: string;
 	markdown_content: RenderableContent;
 };
 declare type PageMetadata = {
 	slug: string;
+	title: string;
 	enable_comment: boolean;
 	allow_index: boolean;
 	navigation_title?: string;
@@ -14,6 +14,7 @@ declare type PageMetadata = {
 declare type Page = Content & PageMetadata;
 declare type PostMetadata = {
 	id: number;
+	title: string;
 	description: string;
 	modified_at: Date;
 	draft: boolean;
