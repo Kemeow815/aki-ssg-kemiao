fc5b904d3ac45dc7c11ec6da37734998932c2c6e
Allenyou <i@allenyou.wang>
2025-06-30 11:35:27 +0800

    refactor: rewrite global layout and 404 page style

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,6 +1,7 @@
 import { config } from "@/data/site-config";
 import type { Metadata } from "next";
 import Link from "next/link";
+import "@/styles/not-found.css";
 
 export const metadata: Metadata = {
 	title: `404 - ${config.blog.title}`,
@@ -8,13 +9,11 @@ export const metadata: Metadata = {
 
 export default async function NotFound() {
 	return (
-		<>
-			<p className="text-3xl font-extrabold my-4">404 Not Found</p>
-			<Link
-				href="/"
-				className="rounded-3xl bg-primary dark:bg-primary/80 dark:text-gray-300 hover:opacity-90 text-white  px-4 py-2 my-4">
+		<div id="not-found-wrap">
+			<p id="not-found-text">404 Not Found</p>
+			<Link id="not-found-button" href="/">
 				返回首页
 			</Link>
-		</>
+		</div>
 	);
 }
