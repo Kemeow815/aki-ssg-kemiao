83288d2473d15ea58238282d3c3953850e643976
Allenyou <i@allenyou.wang>
2025-06-30 17:57:45 +0800

    refactor: rewrite navigation style

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -2,7 +2,6 @@
 "use client";
 import getAvatar from "@/utils/getAvatar";
 import Link from "next/link";
-import { connectString } from "@/utils/connectString";
 import { useAtom, useAtomValue } from "jotai";
 import { darkMode, scrollY } from "@/libs/state-management";
 import {
@@ -10,6 +9,7 @@ import {
 	startTransition,
 	useCallback,
 	useContext,
+	useEffect,
 	useState,
 	useTransition,
 } from "react";
@@ -20,6 +20,7 @@ import {
 	faSun,
 } from "@fortawesome/free-solid-svg-icons";
 import { config } from "@/data/site-config";
+import "@/styles/navigation.css";
 
 function getDarkModeAlt(mode: "auto" | "light" | "dark") {
 	switch (mode) {
@@ -62,7 +63,7 @@ function DarkModeSwitcher() {
 	}, [theme, setTheme]);
 	return (
 		<button
-			className="text-2xl align-baseline w-12 h-12 darkani"
+			id="dark-mode-switcher"
 			onClick={handler}
 			title={`切换深色模式状态（当前：${getDarkModeAlt(theme)}）`}>
 			<FontAwesomeIcon icon={getDarkModeIcon(theme)} />
@@ -75,9 +76,9 @@ const toggleExpandContext = createContext<() => void>(() => {});
 function NavigationItem({ link }: { link: { title: string; url: string } }) {
 	const toggleExpand = useContext(toggleExpandContext);
 	return (
-		<li className="contents" key={link.title}>
+		<li className="navigation-item" key={link.title}>
 			<Link
-				className="h-14 leading-[3.5rem] w-full text-center align-middle block text-xl"
+				className="navigation-link"
 				onClick={() => {
 					toggleExpand();
 				}}
@@ -96,8 +97,12 @@ export default function Navigation({
 	const scroll = useAtomValue(scrollY);
 	const [expanded, setExpanded] = useState(false);
 	const [navHeight, setNavHeight] = useState(5);
+	const [navWidth, setNavWidth] = useState(5);
 	const [menuStep, setMenuStep] = useState<1 | 2 | 3>(1);
 	const [, startTransistion] = useTransition();
+	useEffect(() => {
+		setNavWidth(Math.min(42, 5 * (links.length + 1) + 10));
+	}, [links]);
 	const toggleExpand = useCallback(() => {
 		setExpanded(!expanded);
 		if (!expanded) {
@@ -128,28 +133,24 @@ export default function Navigation({
 				toggleExpand();
 			}}>
 			<nav
-				className="fixed top-0 w-full z-20 justify-center flex bg-none"
-				style={{ "--nav-height": `${navHeight}rem` } as React.CSSProperties}>
-				<div
-					className={connectString([
-						scroll > 500
-							? "w-full top-0 rounded-none"
-							: "rounded-[2.5rem] md:max-w-2xl md:w-auto w-2/3 top-4",
-						"transistion-all transform ease-in-out duration-500",
-						"fixed flex flex-col items-center h-nav md:h-20 overflow-y-hidden flex-shrink-0",
-						"bg-color bg-blur",
-					])}>
-					<div className="h-20 py-2 pl-2 pr-4 w-full flex justify-between items-center gap-8 flex-shrink-0">
+				style={
+					{
+						"--nav-height": `${navHeight}rem`,
+						"--nav-width": `${navWidth}rem`,
+					} as React.CSSProperties
+				}>
+				<div id="navigation-wrap" className={scroll > 500 ? "wide" : ""}>
+					<div id="navigation-bar">
 						<Link href="/">
 							<img
 								src={getAvatar(config.author.email, 80)}
 								width={60}
 								height={60}
 								alt="Avatar"
-								className="rounded-full"
+								style={{ borderRadius: "9999px" }}
 							/>
 						</Link>
-						<ul className="hidden md:flex justify-center gap-8">
+						<ul id="pc-navigation">
 							<li>
 								<Link href="/">首页</Link>
 							</li>
@@ -161,50 +162,38 @@ export default function Navigation({
 								);
 							})}
 						</ul>
-						<div className="flex gap-1">
+						<div id="navigation-tools">
 							<DarkModeSwitcher />
 							<button
-								className="w-12 h-12 md:hidden flex items-center justify-center rounded-full md:-ms-3 transition-colors bg-white bg-opacity-0 active:bg-opacity-10"
+								id="mobile-navigation-toggle"
+								// className="w-12 h-12 md:hidden flex items-center justify-center rounded-full md:-ms-3 transition-colors bg-white bg-opacity-0 active:bg-opacity-10"
 								onClick={() => {
 									toggleExpand();
 								}}>
-								<span className="block relative w-5 h-5" aria-hidden="true">
+								<span id="mobile-navigation-toggle-icon" aria-hidden="true">
 									<span
-										className={connectString([
-											"duration-200 block w-5 h-[0.225rem] bg-black dark:bg-gray-300/80 rounded-full absolute left-1/2 -translate-x-1/2",
-											menuStep === 1 ? "top-0" : "top-1/2 -translate-y-1/2",
-											menuStep === 3 ? "h-[0.2rem] rotate-45 scale-[1.3]" : "",
-										])}></span>
+										className={`mobile-navigation-toggle-bar mobile-navigation-toggle-bar-1 step-${menuStep}`}
+									/>
 									<span
-										className={connectString([
-											"duration-200 w-5 h-[0.225rem] bg-black dark:bg-gray-300/80 rounded-full absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2",
-											menuStep === 3 ? "hidden" : "block",
-										])}></span>
+										className={`mobile-navigation-toggle-bar mobile-navigation-toggle-bar-2 step-${menuStep}`}
+									/>
 									<span
-										className={connectString([
-											"duration-200 block w-5 h-[0.225rem] bg-black dark:bg-gray-300/80 rounded-full absolute left-1/2 -translate-x-1/2",
-											menuStep === 1
-												? "bottom-0"
-												: "bottom-1/2 translate-y-1/2",
-											menuStep === 3 ? "h-[0.2rem] -rotate-45 scale-[1.3]" : "",
-										])}></span>
+										className={`mobile-navigation-toggle-bar mobile-navigation-toggle-bar-3 step-${menuStep}`}
+									/>
 								</span>
 							</button>
 						</div>
 					</div>
 					<hr
-						className={connectString([
-							expanded
-								? "border-black/10 dark:border-white/10"
-								: "border-transparent hidden",
-							"md:hidden w-[calc(100%-1.5rem)] transition-all duration-500 transform ease-in-out",
-						])}
+						id="mobile-navigation-divider"
+						style={{
+							display: expanded ? undefined : "none",
+							borderColor: expanded ? undefined : "transparent",
+						}}
 					/>
 					<ul
-						className={connectString([
-							expanded ? "" : "hidden",
-							"flex flex-col justify-center w-full md:hidden mt-1",
-						])}>
+						id="mobile-navigation"
+						style={{ display: expanded ? undefined : "none" }}>
 						<NavigationItem link={{ title: "首页", url: "/" }} />
 						{links.map((ln) => {
 							return <NavigationItem link={ln} key={ln.title} />;
