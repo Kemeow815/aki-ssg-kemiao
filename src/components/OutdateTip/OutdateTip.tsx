e894ce49b117034bec15f0c336af6953005f01ec
Allenyou <i@allenyou.wang>
2025-07-14 14:48:24 +0800

    fix: outdate tip icon typography missing

--- src/components/OutdateTip/OutdateTip.tsx
+++ src/components/OutdateTip/OutdateTip.tsx
@@ -6,9 +6,12 @@ function Icon() {
 	return (
 		<svg
 			xmlns="http://www.w3.org/2000/svg"
-			width="1em"
-			height="1em"
-			fill="currentColor"
+			style={{
+				width: "1em",
+				height: "1em",
+				fill: "currentColor",
+				display: "inline-block",
+			}}
 			viewBox="0 0 512 512">
 			<path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM216 336l24 0 0-64-24 0c-13.3 0-24-10.7-24-24s10.7-24 24-24l48 0c13.3 0 24 10.7 24 24l0 88 8 0c13.3 0 24 10.7 24 24s-10.7 24-24 24l-80 0c-13.3 0-24-10.7-24-24s10.7-24 24-24zm40-208a32 32 0 1 1 0 64 32 32 0 1 1 0-64z" />
 		</svg>
